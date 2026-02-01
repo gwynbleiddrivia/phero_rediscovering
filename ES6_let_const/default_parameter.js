be3ce27a2634c8f05f0d1bd24caca0c017937c48
gwynbleiddrivia <yasinzme2@gmail.com>
2026-02-01 23:15:53 +0600

    directory name changed, some change in default paramter file

--- ES6_let_const/default_parameter.js
+++
@@ -1,6 +0,0 @@
-function sum (num1, num2){
-    const total = num1 + num2;
-    console.log(total);
-}
-
-sum(10,20 );
---
+++ Mod1/default_parameter.js
@@ -0,0 +1,14 @@
+
+function sum (num1, num2){
+    const total = num1 + num2;
+    console.log(total);
+}
+
+sum(10,20 );
+
+function sum2 (num3, num4=10){
+    const total2 = num3 + num4;
+    console.log(total2);
+}
+
+sum2(10);
