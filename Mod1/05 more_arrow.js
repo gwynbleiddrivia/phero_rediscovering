6f161795e2c2cf55f7f60c369ebf1c73d1cbe037
gwynbleiddrivia <yasinzme2@gmail.com>
2026-02-07 13:12:23 +0600

    more arrow func added, numerics added

---
+++ Mod1/05 more_arrow.js
@@ -0,0 +1,26 @@
+//normal arrow function
+const getTAX = (amount, taxRate) => (amount * taxRate)/100;
+const add = (p, q) => p+q;
+
+
+// single parameter
+
+const getSquare = (x) => x**2;
+getSquare(31); 
+console.log(getSquare(31))
+
+const firstElement = nums => nums[0];
+
+const logIt = (x) => console.log(78)
+console.log(logIt());
+//undefined, coz console log returns nothing
+
+//anonymous function
+//document.getElementById('').addEventListener('click', () => {
+    //anonymous function
+//})
+
+
+//document.getElementById('btn-click').addEventListener('click', event => {
+    // function
+//})
